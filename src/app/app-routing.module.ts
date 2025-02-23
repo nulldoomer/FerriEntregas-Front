006bd72b5b3e5934aa1deb47238e1d2bb65d4366
HuesosAce <pauloarielyepez@gmail.com>
2025-02-23 16:07:13 -0500

    fix: delete unecesarily pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,24 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'distancia',
-    loadChildren: () => import('./distancia/distancia.module').then( m => m.DistanciaPageModule)
-  },
-  {
-    path: 'peso',
-    loadChildren: () => import('./peso/peso.module').then( m => m.PesoPageModule)
-  },
-  {
-    path: 'temperatura',
-    loadChildren: () => import('./temperatura/temperatura.module').then( m => m.TemperaturaPageModule)
   },
 ];
 
