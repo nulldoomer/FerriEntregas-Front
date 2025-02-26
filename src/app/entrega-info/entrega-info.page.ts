89c6600e067ef57d1a62f150495497d50756616f
HuesosAce <pauloarielyepez@gmail.com>
2025-02-25 23:09:34 -0500

    fix: changed entregaInfo modal functionalitty, and googleMaps implementation

--- src/app/entrega-info/entrega-info.page.ts
+++ src/app/entrega-info/entrega-info.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-entrega-info',
@@ -7,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class EntregaInfoPage implements OnInit {
+  @ViewChild('modal', { static: true }) modal!: IonModal;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.openGoogleMapsRoute();
   }
 
+  ionViewWillLeave() {
+    if (this.modal.isOpen) {
+      this.modal.dismiss();
+    }
+  }
+
+  openGoogleMapsRoute() {
+    const origin = 'Ambato,Ecuador';
+    const destination = 'Latacunga,Ecuador';
+    const waypoints = [
+      'Baños,Ecuador',
+      'Pelileo,Ecuador'
+    ];
+
+    const url = this.createGoogleMapsURL(origin, destination, waypoints);
+
+    window.open(url, '_system');
+  }
+
+  createGoogleMapsURL(origin: string, destination: string, waypoints: string[]): string {
+    const waypointsString = waypoints.join('|');
+    return `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&waypoints=${waypointsString}`;
+  }
 }
