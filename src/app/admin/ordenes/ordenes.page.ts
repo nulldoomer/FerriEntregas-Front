45a2d52be5e4ff7d9bc69cfb1e5eac42a5ec8ba9
Darick33 <sebolin2004@gmail.com>
2025-03-20 23:38:11 -0500

    feat(ordenes): add table

--- src/app/admin/ordenes/ordenes.page.ts
+++ src/app/admin/ordenes/ordenes.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MenuController, NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { CustomerService } from 'src/app/services/customer.service';
+import { Customer } from 'src/interfaces/customer.interface';
 
 @Component({
   selector: 'app-ordenes',
@@ -8,9 +15,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenesPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  filterUsers() {
+  throw new Error('Method not implemented.');
   }
+  
+    constructor(    private navController: NavController, private customerService: CustomerService, private menuCtrl: MenuController   ) { }
+  
+    customers: Customer[] = [];
+    displayedColumns: string[] = ['identificacion', 'nombres', 'apellidos',  'acciones'];
+    dataSource = new MatTableDataSource<any>();
+    totalCustomer: number = 0;
+    pageSize: number = 5;
+    pageIndex: number = 0;
+    filterValue: string = ''; 
+    private subscriptions: Subscription = new Subscription(); 
+    
+    @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+    @ViewChild(MatSort) sort: MatSort | undefined;
+    showInfo(pokemon: any) {
+      console.log('Información', pokemon);
+    }
+    onPageChange(event: PageEvent): void {
+      this.pageIndex = event.pageIndex;
+      this.loadcustomers(this.pageIndex + 1, this.filterValue);
+    }
+    info(id:string){
+      this.navController.navigateForward(`admin/customer-form/${id}`);
+    }
+    nuevo(){
+      this.navController.navigateForward('admin/customer-form');
+    
+    }
+    loadcustomers(page: number = 1, filter: string = ''): void {
+      const subscription = this.customerService.getCustomers(page, this.pageSize).subscribe(response => {
+        this.totalCustomer = response.result.totalElements;
+        this.customers = response.result.content; 
+        this.dataSource = new MatTableDataSource(this.customers);
+        this.dataSource.sort = this.sort!;
+        this.dataSource.paginator = this.paginator!;
+        console.log(response);
+      });
+      this.subscriptions.add(subscription); 
+    }
+    searchText: string = '';
+    currentPage: number = 1;
+    itemsPerPage: number = 5;
+    paginatedUsers: any[] = [];
+    totalPages: number | undefined;
+    ngOnInit() {
+      this.menuCtrl.enable(true, 'main-menu'); // Activa el menú al cargar la página
+      this.loadcustomers();
+      if (this.paginator) {
+        this.subscriptions.add(
+          this.paginator.page.subscribe(() => this.onPageChange({ pageIndex: this.paginator!.pageIndex, pageSize: this.pageSize, length: this.totalCustomer }))
+        );
+      }
+  
+      if (this.sort) {
+        this.subscriptions.add(
+          this.sort.sortChange.subscribe(() => {
+            this.dataSource.sort = this.sort!;
+          })
+        );
+      }
+    }
+  
+  
 
 }
---
+++ src/interfaces/ordenes.interface.ts
@@ -0,0 +1,104 @@
+export interface EstimateHour {
+    hour: number;
+    minute: number;
+    second: number;
+    nano: number;
+  }
+  
+  export interface DeliveryStatusName {
+    id: string;
+    deleted: boolean;
+    name: string;
+  }
+  
+  export interface PaymentType {
+    id: string;
+    deleted: boolean;
+    name: string;
+  }
+  
+  export interface Evidence {
+    id: string;
+    deleted: boolean;
+    url: string;
+  }
+  
+  export interface Role {
+    id: string;
+    deleted: boolean;
+    name: string;
+  }
+  
+  export interface Authority {
+    authority: string;
+  }
+  
+  export interface User {
+    id: string;
+    deleted: boolean;
+    firstNames: string;
+    lastNames: string;
+    email: string;
+    password: string;
+    profileImage: string;
+    emailConfirmed: boolean;
+    token: string;
+    roles: Role[];
+    username: string;
+    authorities: Authority[];
+    enabled: boolean;
+    accountNonLocked: boolean;
+    accountNonExpired: boolean;
+    credentialsNonExpired: boolean;
+  }
+  
+  export interface Customer {
+    id: string;
+    deleted: boolean;
+    firstNames: string;
+    lastNames: string;
+    email: string;
+    password: string;
+    profileImage: string;
+    emailConfirmed: boolean;
+    token: string;
+    roles: Role[];
+    identification: string;
+    address: string;
+    addressMaps: string;
+    phone: string;
+    birthDate: string;
+    username: string;
+    authorities: Authority[];
+    enabled: boolean;
+    accountNonLocked: boolean;
+    accountNonExpired: boolean;
+    credentialsNonExpired: boolean;
+  }
+  
+  export interface Invoice {
+    id: string;
+    numeration: string;
+    invoiceNumber: string;
+    deliveryDate: string;
+    estimateHourInit: EstimateHour;
+    estimateHourEnd: EstimateHour;
+    deliveryStatusName: DeliveryStatusName;
+    paymentType: PaymentType;
+    credit: number;
+    total: number;
+    evidence: Evidence[];
+    user: User;
+    customer: Customer;
+    deliveryData: string;
+    observations: string;
+    comments: string;
+  }
+  
+  export interface ApiResponse {
+    result: Invoice;
+    errors: string[];
+    code: number;
+    success: boolean;
+  }
+  
