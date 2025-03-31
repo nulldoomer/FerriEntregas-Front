e7aec4b9cccda2068f12c7d82edd6727b05ae485
Darick33 <sebolin2004@gmail.com>
2025-03-31 02:00:12 -0500

    feat(Entregas-Info): add mapa segun la direccion del cliente

--- src/app/admin/entrega-info/entrega-info.page.ts
+++ src/app/admin/entrega-info/entrega-info.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IonModal, NavController } from '@ionic/angular';
+import { OrdenesService } from 'src/app/services/ordenes.service';
 import { OcrService } from 'src/app/services/ors.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-entrega-info',
@@ -10,20 +13,64 @@ import { OcrService } from 'src/app/services/ors.service';
 })
 export class EntregaInfoPage implements OnInit {
   @ViewChild('modal', { static: true }) modal!: IonModal;
-  extractedText: string | null = null;  // Variable para almacenar el texto extraído
+  extractedText: string | null = null;
+  orden: any;
+  id: string = '';
+  rutaA: string = '';  // Latitud
+  rutaB: string = '';  // Longitud
+  googleMapsUrl: SafeResourceUrl | null = null; // Inicialmente nulo
 
   constructor(
     private navController: NavController,
-    private ocrService: OcrService  // Inyecta el servicio OCR
+    private ocrService: OcrService,
+    private ordenesService: OrdenesService,
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    this.getOrden(this.id);
+  }
 
-  ionViewWillLeave() {
-    if (this.modal.isOpen) {
-      this.modal.dismiss();
+  getOrden(id: string) {
+    this.ordenesService.getCustomerById(id).subscribe(response => {
+      this.orden = response.result;
+        console.log(response.result);
+      // Aseguramos que los valores existen antes de usarlos
+      if (response.result.customer.addressMaps) {
+        const addressParts = response.result.customer.addressMaps.split('=');
+          this.rutaA = addressParts[1];
+          
+          this.rutaB = this.rutaA.split(',')[1];
+          this.rutaA = this.rutaA.split(',')[0];
+
+          console.log("Latitud:", this.rutaA);
+          console.log("Longitud:", this.rutaB);
+
+          // Solo establecer la URL cuando los datos estén listos
+          this.setGoogleMapsUrl();
+      } else {
+        console.error("No se encontró addressMaps en la respuesta");
+      }
+    });
+  }
+  // Latitud: 1.2365728
+  // entrega-info.page.ts:48 Longitud: 78.6214727
+  setGoogleMapsUrl() {
+    if (this.rutaA && this.rutaB) {
+      const unsafeUrl = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d996.1676125164406!2d${this.rutaB}!3d${this.rutaA}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zLTEuMjM2NTcyOCwgLTc4LjYyMTQ3Mjc!5e0!3m2!1ses!2sec!4v1710113044864!5m2!1ses!2sec`;
+      console.log("URL de Google Maps:", unsafeUrl);
+      
+      // Sanitizar la URL
+      this.googleMapsUrl = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
+    } else {
+      console.error("Coordenadas no definidas correctamente");
     }
   }
+  
+  // <!-- https://www.google.com/maps?q=-1.2365728,-78.6214727 -->
+
 
   goBackInicio() {
     this.navController.back();
@@ -31,28 +78,58 @@ export class EntregaInfoPage implements OnInit {
 
   makeCall() {
     const phoneNumber = '593983224738';  // Número con el que se hará la llamada
-    window.location.href = `tel:${phoneNumber}`;
+    let telefono = this.orden.customer.phone;
+    if(telefono.startsWith('0')){
+
+      telefono = '593' + telefono.split('0')[1] ;
+    }
+    window.location.href = `tel:${telefono
+    }`;
+  }
+  ionViewWillLeave() {
+    if (this.modal.isOpen) {
+      this.modal.dismiss();
+    }
   }
 
   sendWhatsAppMessage() {
     const phoneNumber = '593983224738';  // Número al que se enviará el mensaje
-    window.location.href = `https://wa.me/${phoneNumber}`;
+    let telefono = this.orden.customer.phone;
+    if(telefono.startsWith('0')){
+
+      telefono = '593' + telefono.split('0')[1] ;
+    }
+    window.location.href = `https://wa.me/${telefono}`;
   }
 
   openGoogleMapsRoute() {
-    const origin = 'Ambato,Ecuador';
-    const destination = 'Latacunga,Ecuador';
-    const waypoints = ['Baños,Ecuador', 'Pelileo,Ecuador'];
-
-    const url = this.createGoogleMapsURL(origin, destination, waypoints);
-
-    window.open(url, '_system');
+    if (this.rutaA && this.rutaB) {
+      // Definir el destino usando las coordenadas
+      const destination = `${this.rutaA},${this.rutaB}`; // Usamos las coordenadas
+  
+      // Crear la URL de Google Maps con las coordenadas
+      const url = this.createGoogleMapsURL(destination);
+  
+      // Abrir el enlace en una nueva ventana (o sistema)
+      window.open(url, '_system');
+    } else {
+      console.error("No se han definido correctamente las coordenadas de la ruta.");
+    }
   }
+  
+  
+  
+  
 
-  createGoogleMapsURL(origin: string, destination: string, waypoints: string[]): string {
-    const waypointsString = waypoints.join('|');
-    return `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&waypoints=${waypointsString}`;
+  createGoogleMapsURL(destination: string): string {
+    // Usar solo las coordenadas en formato latitud,longitud
+    // https://www.google.com/maps?q=-1.2366603,-78.6217015
+
+    return `https://www.google.com/maps?q=${destination}`;
   }
+  
+  
+  
 
   openFileInput() {
     const fileInput = document.createElement('input');
